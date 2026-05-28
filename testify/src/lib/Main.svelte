5c4ae19568a1dc17aae921364acfbf446ef8981a
Alexey Khoroshenkiy <alexeyk4@nycstudents.net>
2026-05-28 14:03:32 -0400

    styles

--- testify/src/lib/Main.svelte
+++ testify/src/lib/Main.svelte
@@ -6,11 +6,11 @@
 <h1>Welcome to testify!</h1>
 
 <form id="form" class="main">
-  <input bind:value={subject} />
-  <select bind:value={questionAmount}>
+  <textarea bind:value={subject} class="main__textarea--subject"> </textarea>
+  <select bind:value={questionAmount} class="main__select--questions">
     {#each Array(5) as _, i}
       <option value={(i + 1) * 10}>{(i + 1) * 10}</option>
     {/each}
   </select>
-  <button type="submit" class="send">↑</button>
+  <button type="submit" class="main__button--send">↑</button>
 </form>
